219d80d356d593bcd09732ff2a1483d4a22f038b
Bernhard Schiehl <bernhard.schiehl@chello.at>
2023-09-28 14:17:29 +0200

    update hyperparameters

--- pac3man/reinforcement/TrainingParameters.py
+++ pac3man/reinforcement/TrainingParameters.py
@@ -85,9 +85,12 @@ class TrainingParameters:
         self.beta = float(self.beta)
         self.reward_size = int(self.reward_size)
         self.constraint = float(self.constraint)
-        self.no_cuda = self.no_cuda == "True"
-        self.largeEnv = self.largeEnv == "True"
-        self.trained = self.trained == "True"
+        if type(self.no_cuda) == str:
+            self.no_cuda = self.no_cuda == "True"
+        if type(self.largeEnv) == str:
+            self.largeEnv = self.largeEnv == "True"
+        if type(self.trained) == str:
+            self.trained = self.trained == "True"
         self.lextab_on_policy = self.lextab_on_policy == "True"
         # assert (self.agent_name in agent_names)
         # assert (self.env_name in env_names)
